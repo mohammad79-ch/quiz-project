b4774c7e831e94c8aca07c2a4c9f17f1696284ab
Mohamamd <and>
2021-09-06 11:49:21 +0430

    create relstionship between models

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title','rate','level','person_id'];
+
+    public function subQuestion()
+    {
+        return $this->hasMany(SubQuestion::class,'question_id');
+    }
+
+    public function user()
+    {
+        return $this->hasMany(User::class,'person_id');
+    }
+
+
+
 }
--- app/Models/SubQuestion.php
+++ app/Models/SubQuestion.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class SubQuestion extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title','is_answer','question_id'];
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class,'sub_question_user');
+    }
+
 }
